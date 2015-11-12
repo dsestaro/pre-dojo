edf016bef104aded8a9d230665ba0d2f653d2364
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 21:47:51 -0200

    criacao do teste de inicializacao de partidas

---
+++ predojo/src/test/java/com/br/amil/matchstatisctics/MatchStatisticsTest.java
@@ -0,0 +1,38 @@
+package com.br.amil.matchstatisctics;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.br.amil.predojo.start.PredojoApplication;
+import com.jayway.restassured.RestAssured;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PredojoApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class MatchStatisticsTest {
+
+	@Value("${local.server.port}")
+	private int port;
+	
+	@Before
+	public void setUp() {
+		RestAssured.port = port;
+	}
+	
+	@Test
+	public void createMatchTest() {
+		MatchStatistics matchstatistics = new MatchStatistics();
+
+		Match answer = new Match();
+		
+		Assert.assertEquals(answer, matchstatistics.createMatch());
+	}
+}
