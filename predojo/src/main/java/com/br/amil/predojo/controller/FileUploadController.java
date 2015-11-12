c9b5b3ddb56dc40a799149f903f19b4792e0c6a9
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 00:53:23 -0200

    Devido ao problema do rest assured utilizar multpart foi criado um metodo que recebe string para que ele seja coberto nos testes

--- predojo/src/main/java/com/br/amil/predojo/controller/FileUploadController.java
+++ predojo/src/main/java/com/br/amil/predojo/controller/FileUploadController.java
@@ -3,6 +3,8 @@ package com.br.amil.predojo.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.br.amil.predojo.configs.FileConfig;
+
 @Controller
 public class FileUploadController {
 
@@ -20,12 +24,31 @@ public class FileUploadController {
 		return "OK";
 	}
 	
-	@RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
-	public @ResponseBody String handleFileUpload(@RequestParam("file") String file) {
+	/**
+	 * Metodo criado para a realizacao de testes devido a limitacoes encontradas no Rest Assured
+	 * 
+	 * @param file
+	 * @return
+	 */
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public @ResponseBody String handleUpload(@RequestParam("file") String file) {
 		if (!file.isEmpty()) {
 			return "OK";
 		} else {
 			return "Empty File!";
+		}
+	}
+	
+	@RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
+	public @ResponseBody String handleFileUpload(@RequestParam("file") MultipartFile file) {
+		try {
+			File tempFile = File.createTempFile(file.getOriginalFilename(), FileConfig.PREFIX);
+			
+			file.transferTo(tempFile);
+			
+			return handleUpload(Files.readAllBytes(tempFile.toPath()).toString());
+		} catch (IllegalStateException | IOException e) {
+			return "Ocorreu um erro ao processar o arquivo!";
 		}
 	}
 }
