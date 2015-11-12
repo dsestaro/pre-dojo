15c6602d9371cdbc7bc6986dd636d1b224e502ff
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 13:39:48 -0200

    Alteracao do retorno para um objeto e primeira regra de negocio para separar o tempo

--- predojo/src/main/java/com/br/amil/predojo/matchstatistics/FileInterpreter.java
+++ predojo/src/main/java/com/br/amil/predojo/matchstatistics/FileInterpreter.java
@@ -1,9 +1,15 @@
 package com.br.amil.predojo.matchstatistics;
 
+import com.br.amil.predojo.configs.FileConstants;
+
 public class FileInterpreter {
 	
-	public static String[] parseLine(String line) {
-		
+	public static LineInformation parseLine(String line) {
+		if(!line.isEmpty()) {
+			String[] splitedLine = line.split(FileConstants.TIME_SEPARATOR);
+			
+			
+		}
 		
 		return null;
 	}
