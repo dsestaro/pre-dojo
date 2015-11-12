0483a5d0e3e87f1b88d64676c2bfbbc6627153d8
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 15:29:12 -0200

    Implementacao da regra para pegar o tipo de entrada na linha do arquivo

--- predojo/src/main/java/com/br/amil/predojo/matchstatistics/FileInterpreter.java
+++ predojo/src/main/java/com/br/amil/predojo/matchstatistics/FileInterpreter.java
@@ -5,6 +5,7 @@ import java.text.ParseException;
 import com.br.amil.predojo.configs.FileConfig;
 import com.br.amil.predojo.configs.FileConstants;
 import com.br.amil.predojo.matchstatistics.dto.LineInformation;
+import com.br.amil.predojo.matchstatistics.enums.MatchStatusEnum;
 import com.br.amil.predojo.utils.FileUtils;
 
 public class FileInterpreter {
@@ -14,7 +15,9 @@ public class FileInterpreter {
 		
 		if(!line.isEmpty()) {
 			try {
-				splitTime(line, lineInformation);
+				line = splitTime(line, lineInformation);
+				
+				line = splitStatus(line, lineInformation);
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
@@ -23,11 +26,47 @@ public class FileInterpreter {
 		return lineInformation;
 	}
 
-	private static void splitTime(String line, LineInformation lineInformation) throws ParseException {
+	private static String splitStatus(String line, LineInformation lineInformation) {
+		String[] splitedLine;
+		
+		if(line.contains(FileConstants.NEW_MATCH)) {
+			splitStatus(line, lineInformation, FileConstants.NEW_MATCH, MatchStatusEnum.STARTED);
+		} else if(line.contains(FileConstants.MATCH_ENDED)) {
+			splitStatus(line, lineInformation, FileConstants.MATCH_ENDED, MatchStatusEnum.ENDED);
+		} else if(line.contains(FileConstants.KILLED)) {
+			splitStatus(line, lineInformation, FileConstants.KILLED, MatchStatusEnum.KILLED);
+			
+			splitPlayers(line, lineInformation);
+		}
+		
+		return null;
+	}
+
+	private static void splitPlayers(String line, LineInformation lineInformation) {
+		String[] splitedLine = line.split(FileConstants.KILLED);
+		
+		if(FileUtils.validateSplit(splitedLine)) {
+
+		}
+	}
+
+	private static void splitStatus(String line, LineInformation lineInformation, String constant, MatchStatusEnum type) {
+		String[] splitedLine = line.split(constant);
+		
+		if(FileUtils.validateSplit(splitedLine)) {
+			lineInformation.setStatus(type);
+		}
+	}
+
+	private static String splitTime(String line, LineInformation lineInformation) throws ParseException {
 		String[] splitedLine = line.split(FileConstants.TIME_SEPARATOR);
 		
 		if(FileUtils.validateSplit(splitedLine)) {
 			lineInformation.setTime(FileConfig.FORMATTER.parse(splitedLine[0]));
+			
+			return splitedLine[1];
 		}
+		
+		return null;
 	}
 }
