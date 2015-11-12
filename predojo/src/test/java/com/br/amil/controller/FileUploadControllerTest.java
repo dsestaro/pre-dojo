d54aab66e6c33c564035a2192dae303664f59f66
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 00:29:12 -0200

    Adicao do teste para saber se o servico esta funcionando Alteracao do metodo de teste do upload de arquivo

--- predojo/src/test/java/com/br/amil/controller/FileUploadControllerTest.java
+++ predojo/src/test/java/com/br/amil/controller/FileUploadControllerTest.java
@@ -4,31 +4,54 @@ import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.br.amil.predojo.start.PredojoApplication;
+import com.jayway.restassured.RestAssured;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PredojoApplication.class)
 @WebAppConfiguration
+@IntegrationTest("server.port:0")
 public class FileUploadControllerTest {
 
+	@Value("${local.server.port}")
+	private int port;
+	
+	@Before
+	public void setUp() {
+		RestAssured.port = port;
+	}
+	
 	@Test
-	public void uploadFile() {
-		File testFile = Mockito.mock(File.class);
+	public void uploadFileTest() throws IOException {
+		File testFile = File.createTempFile("game", ".txt");
 
 		given().
-		         multiPart(testFile).
+		        param("file", Files.readAllBytes(testFile.toPath()).toString()).
 		expect().
-		         body("fileUploadResult", is("OK")).
+		        body(is("OK")).
 		when().
-		         post("/fileUpload");
+		        post("/fileUpload");
+	}
+	
+	@Test
+	public void isAliveTest() {
+		given().
+		expect().
+		         body(is("OK")).
+		when().
+		         get("/isAlive");
 	}
 }
 
