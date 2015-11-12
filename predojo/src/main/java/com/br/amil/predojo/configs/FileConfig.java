d8a1d5fa48c9c28dde11d600dd13935526ebc27c
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 14:08:37 -0200

    Adicao de uma constante para realizar o parse da data

--- predojo/src/main/java/com/br/amil/predojo/configs/FileConfig.java
+++ predojo/src/main/java/com/br/amil/predojo/configs/FileConfig.java
@@ -1,5 +1,9 @@
 package com.br.amil.predojo.configs;
 
+import java.text.SimpleDateFormat;
+
 public class FileConfig {
-	public static final String PREFIX = ".txt"; 
+	public static final String PREFIX = ".txt";
+	
+	public static final SimpleDateFormat FORMATTER = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 }
