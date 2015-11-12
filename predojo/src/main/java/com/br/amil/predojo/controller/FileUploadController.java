618900decef22446e4f2e082abc9d2050ec71f9d
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 00:30:36 -0200

    Criacao do metodo de upload de arquivo (versao inicial de teste)

--- predojo/src/main/java/com/br/amil/predojo/controller/FileUploadController.java
+++ predojo/src/main/java/com/br/amil/predojo/controller/FileUploadController.java
@@ -1,9 +1,16 @@
 package com.br.amil.predojo.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileUploadController {
@@ -13,5 +20,20 @@ public class FileUploadController {
 		return "OK";
 	}
 	
-	
+	@RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
+	public @ResponseBody String handleFileUpload(@RequestParam("file") String file) {
+		if (!file.isEmpty()) {
+			try {
+				byte[] bytes = file.getBytes();
+				BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("teste")));
+				stream.write(bytes);
+				stream.close();
+				return "OK";
+			} catch (Exception e) {
+				return "Fail";
+			}
+		} else {
+			return "Empty File!";
+		}
+	}
 }
