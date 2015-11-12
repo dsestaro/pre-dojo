c45b4a6206ded8f0cb6eaf0682f425e8358d02c4
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 14:09:06 -0200

    Atualizacao do teste para tratar o retorno campo a campo e nao referencia de memoria

--- predojo/src/test/java/com/br/amil/matchstatisctics/MatchStatisticsTest.java
+++ predojo/src/test/java/com/br/amil/matchstatisctics/MatchStatisticsTest.java
@@ -1,5 +1,8 @@
 package com.br.amil.matchstatisctics;
 
+import java.text.ParseException;
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +13,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.br.amil.predojo.configs.FileConfig;
 import com.br.amil.predojo.matchstatistics.FileInterpreter;
+import com.br.amil.predojo.matchstatistics.dto.LineInformation;
 import com.br.amil.predojo.start.PredojoApplication;
 import com.jayway.restassured.RestAssured;
 
@@ -29,11 +34,19 @@ public class MatchStatisticsTest {
 	}
 	
 	@Test
-	public void parseLine() {
+	public void parseLine() throws ParseException {
 		String line = "23/04/2013 15:34:22 - New match 11348965 has started";
 		
 		LineInformation answer = new LineInformation();
+		answer.setTime(FileConfig.FORMATTER.parse("23/04/2013 15:34:22"));
 		
-		Assert.assertArrayEquals(answer, FileInterpreter.parseLine(line));
+		LineInformation response = FileInterpreter.parseLine(line);		
+		
+		Assert.assertEquals(answer.getMatch(), response.getMatch());
+		Assert.assertEquals(answer.getPlayerOne(), response.getPlayerOne());
+		Assert.assertEquals(answer.getPlayerTwo(), response.getPlayerTwo());
+		Assert.assertEquals(answer.getWeapon(), response.getWeapon());
+		Assert.assertEquals(answer.getStatus(), response.getStatus());
+		Assert.assertEquals(answer.getTime(), response.getTime());
 	}
 }
