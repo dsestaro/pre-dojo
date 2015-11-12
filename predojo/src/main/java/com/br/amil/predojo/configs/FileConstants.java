78d09a2e1b701764a3edfe936e444c3e4eb5a758
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 21:42:36 -0200

    Remocao de um espaco em branco incorreto nas constantes

--- predojo/src/main/java/com/br/amil/predojo/configs/FileConstants.java
+++ predojo/src/main/java/com/br/amil/predojo/configs/FileConstants.java
@@ -3,7 +3,8 @@ package com.br.amil.predojo.configs;
 public class FileConstants {
 	public final static String TIME_SEPARATOR = " - ";
 	public final static String SPACE = " ";
-	public final static String NEW_MATCH = " New match ";
+	public final static String NEW_MATCH = "New match ";
 	public final static String KILLED = " killed ";
 	public final static String MATCH_ENDED = " has ended";
+	public final static String WORLD = "<WORLD>";
 }
