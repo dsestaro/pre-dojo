858cc45dc55dd5591594cda4fbba8e98fb25b060
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 14:09:47 -0200

    Desenvolvimento do parse da linha

--- predojo/src/main/java/com/br/amil/predojo/matchstatistics/FileInterpreter.java
+++ predojo/src/main/java/com/br/amil/predojo/matchstatistics/FileInterpreter.java
@@ -1,16 +1,33 @@
 package com.br.amil.predojo.matchstatistics;
 
+import java.text.ParseException;
+
+import com.br.amil.predojo.configs.FileConfig;
 import com.br.amil.predojo.configs.FileConstants;
+import com.br.amil.predojo.matchstatistics.dto.LineInformation;
+import com.br.amil.predojo.utils.FileUtils;
 
 public class FileInterpreter {
 	
 	public static LineInformation parseLine(String line) {
+		LineInformation lineInformation = new LineInformation();
+		
 		if(!line.isEmpty()) {
-			String[] splitedLine = line.split(FileConstants.TIME_SEPARATOR);
-			
-			
+			try {
+				splitTime(line, lineInformation);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
 		}
 		
-		return null;
+		return lineInformation;
+	}
+
+	private static void splitTime(String line, LineInformation lineInformation) throws ParseException {
+		String[] splitedLine = line.split(FileConstants.TIME_SEPARATOR);
+		
+		if(FileUtils.validateSplit(splitedLine)) {
+			lineInformation.setTime(FileConfig.FORMATTER.parse(splitedLine[0]));
+		}
 	}
 }
