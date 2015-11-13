cb2bc6384bcf9a929519de1d4aeba0c71dceb082
Davidson Sestaro <dsestaro@gmail.com>
2015-11-13 02:35:20 -0200

    Alteracao da tela para conter as tabelas com os resultados

--- predojo/src/main/webapp/js/controller/index-controller.js
+++ predojo/src/main/webapp/js/controller/index-controller.js
@@ -1,13 +1,26 @@
 //inject angular file upload directives and services.
 var app = angular.module('fileUpload', [ 'ngFileUpload' ]);
 
+app.filter('orderObjectBy', function() {
+	  return function(items, field, reverse) {
+		    var filtered = [];
+		    angular.forEach(items, function(item) {
+		      filtered.push(item);
+		    });
+		    filtered.sort(function (a, b) {
+		      return (a[field] > b[field] ? 1 : -1);
+		    });
+		    if(reverse) filtered.reverse();
+		    return filtered;
+		  };
+		});
 app.controller('IndexController', [
 		'$scope',
 		'Upload',
 		'$timeout',
 		function($scope, Upload, $timeout) {
 			$scope.matches;
-			
+
 			$scope.upload = function(file) {
 				file.upload = Upload.upload({
 					url : 'http://localhost:8080/fileUpload',
@@ -16,10 +29,10 @@ app.controller('IndexController', [
 					},
 				}).then(
 						function(response) {
-							$scope.matches = response.data; 
+							$scope.matches = response.data;
 						},
 						function(response) {
-							console.error(response); 
+							console.error(response);
 						},
 						function(evt) {
 							// Math.min is to fix IE which reports 200%
