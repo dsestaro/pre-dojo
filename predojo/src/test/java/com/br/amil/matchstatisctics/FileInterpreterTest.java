db0a1d31f35612c85dedc336e8302d11686e8ffa
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 23:04:17 -0200

    Criacao do teste para processamento de todas as linhas

---
+++ predojo/src/test/java/com/br/amil/matchstatisctics/FileInterpreterTest.java
@@ -0,0 +1,45 @@
+package com.br.amil.matchstatisctics;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.br.amil.predojo.matchstatistics.FileInterpreter;
+import com.br.amil.predojo.start.PredojoApplication;
+import com.google.common.io.CharStreams;
+import com.jayway.restassured.RestAssured;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PredojoApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class FileInterpreterTest {
+
+	@Value("${local.server.port}")
+	private int port;
+	
+	@Before
+	public void setUp() {
+		RestAssured.port = port;
+	}
+	
+	@Test
+	public void processLines() throws IOException {
+		InputStream inputStream = this.getClass().getResourceAsStream("/game.txt");
+		
+		String file = CharStreams.toString(new InputStreamReader(inputStream));
+		
+		String[] lines = file.split("\n\r");
+		
+		FileInterpreter.parseLines(lines);
+	}
+}
