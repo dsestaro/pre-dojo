f351edbb21d24e97cf6dc389665f48a8c11aaacc
Davidson Sestaro <dsestaro@gmail.com>
2015-11-13 02:39:20 -0200

    Adicao da arma preferida de cada jogador

--- predojo/src/main/java/com/br/amil/predojo/match/dto/PlayerInfoDTO.java
+++ predojo/src/main/java/com/br/amil/predojo/match/dto/PlayerInfoDTO.java
@@ -8,11 +8,15 @@ public class PlayerInfoDTO {
 	private int playersKills;
 	private Map<String, WeaponInfoDTO> weaponsKills;
 	private String playerName;
+	private int prefWeaponKills;
+	private String prefWeapon;
 	
 	public PlayerInfoDTO(String name) {
 		this.playerName = name;
 		this.playersDeath = 0;
 		this.playersKills = 0;
+		this.prefWeaponKills = 0;
+		this.prefWeapon = "";
 		this.weaponsKills = new HashMap<String, WeaponInfoDTO>();
 	}
 	public int getPlayersDeath() {
@@ -34,6 +38,11 @@ public class PlayerInfoDTO {
 		
 		int killsNbr = this.weaponsKills.get(name).getKills();
 		
+		if(killsNbr + 1 > this.prefWeaponKills) {
+			this.prefWeaponKills = killsNbr + 1;
+			this.prefWeapon = name;
+		}
+		
 		this.weaponsKills.get(name).setKills(killsNbr + 1);
 	}
 	public Map<String, WeaponInfoDTO> getWeaponsKills() {
@@ -44,5 +53,20 @@ public class PlayerInfoDTO {
 	}
 	public void setPlayerName(String playerName) {
 		this.playerName = playerName;
+	}
+	public int getPrefWeaponKills() {
+		return prefWeaponKills;
+	}
+	public void setPrefWeaponKills(int prefWeaponKills) {
+		this.prefWeaponKills = prefWeaponKills;
+	}
+	public String getPrefWeapon() {
+		return prefWeapon;
+	}
+	public void setPrefWeapon(String prefWeapon) {
+		this.prefWeapon = prefWeapon;
+	}
+	public void setWeaponsKills(Map<String, WeaponInfoDTO> weaponsKills) {
+		this.weaponsKills = weaponsKills;
 	}
 }
