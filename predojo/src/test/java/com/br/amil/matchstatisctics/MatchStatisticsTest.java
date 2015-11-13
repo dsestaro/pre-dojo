a4874c4a17606efd0c8e6b0edc41b6deb32d51a5
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 22:27:35 -0200

    Adicao dos parametros, aos testes, a serem utilizados no momento da criacao de uma match

--- predojo/src/test/java/com/br/amil/matchstatisctics/MatchStatisticsTest.java
+++ predojo/src/test/java/com/br/amil/matchstatisctics/MatchStatisticsTest.java
@@ -1,5 +1,7 @@
 package com.br.amil.matchstatisctics;
 
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,9 +12,10 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.br.amil.predojo.configs.FileConfig;
+import com.br.amil.predojo.match.Match;
 import com.br.amil.predojo.matchstatistics.MatchStatistics;
 import com.br.amil.predojo.start.PredojoApplication;
-import com.fasterxml.jackson.databind.deser.DataFormatReaders.Match;
 import com.jayway.restassured.RestAssured;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,10 +34,13 @@ public class MatchStatisticsTest {
 	
 	@Test
 	public void createMatchTest() {
+		String id = "11348965";
+		Date dateStart = FileConfig.FORMATTER.parse("23/04/2013 15:34:22");
+		
 		MatchStatistics matchstatistics = new MatchStatistics();
 
 		Match answer = new Match();
 		
-		Assert.assertEquals(answer, matchstatistics.createMatch());
+		Assert.assertEquals(answer, matchstatistics.createMatch(id, dateStart));
 	}
 }
