289048704e8c608c1d77a375d5418b286616bc56
Davidson Sestaro <dsestaro@gmail.com>
2015-11-13 01:13:31 -0200

    refactor do controller js

--- predojo/src/main/webapp/js/controller/index-controller.js
+++ predojo/src/main/webapp/js/controller/index-controller.js
@@ -1,37 +1,32 @@
 //inject angular file upload directives and services.
 var app = angular.module('fileUpload', [ 'ngFileUpload' ]);
 
-app
-		.controller(
-				'IndexController',
-				[
-						'$scope',
-						'Upload',
-						'$timeout',
-						function($scope, Upload, $timeout) {
-							$scope.upload = function(file) {
-								file.upload = Upload
-										.upload({
-											url : 'http://localhost:8080/fileUpload',
-											data : {
-												file : file,
-											},
-										});
+app.controller('IndexController', [
+		'$scope',
+		'Upload',
+		'$timeout',
+		function($scope, Upload, $timeout) {
+			$scope.matches;
+			
+			$scope.upload = function(file) {
+				file.upload = Upload.upload({
+					url : 'http://localhost:8080/fileUpload',
+					data : {
+						file : file,
+					},
+				}).then(
+						function(response) {
+							$scope.matches = response.data; 
+						},
+						function(response) {
+							console.error(response); 
+						},
+						function(evt) {
+							// Math.min is to fix IE which reports 200%
+							// sometimes
+							file.progress = Math.min(100, parseInt(100.0
+									* evt.loaded / evt.total));
+						});
 
-								file.upload.then(function(response) {
-									$timeout(function() {
-										file.result = response.data;
-									});
-								}, function(response) {
-									if (response.status > 0)
-										$scope.errorMsg = response.status
-												+ ': ' + response.data;
-								}, function(evt) {
-									// Math.min is to fix IE which reports 200%
-									// sometimes
-									file.progress = Math.min(100,
-											parseInt(100.0 * evt.loaded
-													/ evt.total));
-								});
-							}
-						} ]);
+			}
+		} ]);
