d07c75bc3aaf8c94223235c67641338576f7bd32
Davidson Sestaro <dsestaro@gmail.com>
2015-11-12 22:46:04 -0200

    Adicao do Guava ao POM e adicao do enconding aos file configs

--- predojo/src/main/java/com/br/amil/predojo/configs/FileConfig.java
+++ predojo/src/main/java/com/br/amil/predojo/configs/FileConfig.java
@@ -4,6 +4,6 @@ import java.text.SimpleDateFormat;
 
 public class FileConfig {
 	public static final String PREFIX = ".txt";
-	
+	public static final String ENCODING = "UTF-8";
 	public static final SimpleDateFormat FORMATTER = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 }
